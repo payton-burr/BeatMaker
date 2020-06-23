60580cefe38f438ff6ce7a8bc5382a62cb206a44
Payton Burr <36023709+payton-burr@users.noreply.github.com>
2020-06-23 14:55:56 -0600

    fixed missing html elements, added drumkit class to create loop over pads and sets active class onclick

---
+++ app.js
@@ -0,0 +1,40 @@
+class DrumKit {
+  constructor() {
+    this.pads = document.querySelectorAll('.pad');
+    this.playButton = document.querySelector('.play');
+    this.kickAudio = document.querySelector('.kick-sound');
+    this.snareAudio = document.querySelector('.snare-sound');
+    this.hihatAudio = document.querySelector('.hihat-sound');
+    this.index = 0;
+    this.bpm = 150;
+  }
+
+  activePad() {
+    this.classList.toggle('active');
+  }
+
+  repeat() {
+    let step = this.index % 8;
+    const activeBeat = document.querySelectorAll(`.b${step}`);
+    console.log(step);
+    this.index++;
+  }
+
+  start() {
+    const interval = (60 / this.bpm) * 1000;
+
+    setInterval(() => {
+      this.repeat();
+    }, interval);
+  }
+}
+
+const drumKit = new DrumKit();
+
+drumKit.pads.forEach((pad) => {
+  pad.addEventListener('click', drumKit.activePad);
+});
+
+drumKit.playButton.addEventListener('click', function () {
+  drumKit.start();
+});
